68871c9992f30ee4debcf14d741744b278c24f4e
tim <rosstimo@isu.edu>
2023-03-08 08:42:28 -0700

    add menu

--- WinFormControls/ControlsForm.Designer.vb
+++ WinFormControls/ControlsForm.Designer.vb
@@ -28,6 +28,17 @@ Partial Class ControlsForm
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TopMenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.HelpTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TopMenuStrip.SuspendLayout()
         Me.SuspendLayout()
         '
         'Button1
@@ -77,6 +88,83 @@ Partial Class ControlsForm
         Me.TextBox4.Size = New System.Drawing.Size(150, 31)
         Me.TextBox4.TabIndex = 0
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(86, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 25)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(86, 81)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 25)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(86, 118)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 25)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(86, 155)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(63, 25)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Label4"
+        '
+        'Label5
+        '
+        Me.Label5.Location = New System.Drawing.Point(33, 203)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(285, 148)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "lkAJSHDLKJASHD"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(324, 62)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(449, 289)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
+        '
+        'TopMenuStrip
+        '
+        Me.TopMenuStrip.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.TopMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HelpTopMenuItem})
+        Me.TopMenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.TopMenuStrip.Name = "TopMenuStrip"
+        Me.TopMenuStrip.Size = New System.Drawing.Size(800, 33)
+        Me.TopMenuStrip.TabIndex = 11
+        Me.TopMenuStrip.Text = "MenuStrip1"
+        '
+        'HelpTopMenuItem
+        '
+        Me.HelpTopMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutTopMenuItem})
+        Me.HelpTopMenuItem.Name = "HelpTopMenuItem"
+        Me.HelpTopMenuItem.Size = New System.Drawing.Size(65, 29)
+        Me.HelpTopMenuItem.Text = "&Help"
+        '
+        'AboutTopMenuItem
+        '
+        Me.AboutTopMenuItem.Name = "AboutTopMenuItem"
+        Me.AboutTopMenuItem.Size = New System.Drawing.Size(270, 34)
+        Me.AboutTopMenuItem.Text = "&About"
+        '
         'ControlsForm
         '
         Me.AcceptButton = Me.Button2
@@ -84,14 +172,25 @@ Partial Class ControlsForm
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CancelButton = Me.Button1
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox4)
         Me.Controls.Add(Me.TextBox3)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TopMenuStrip)
+        Me.MainMenuStrip = Me.TopMenuStrip
         Me.Name = "ControlsForm"
         Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TopMenuStrip.ResumeLayout(False)
+        Me.TopMenuStrip.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -103,4 +202,13 @@ Partial Class ControlsForm
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents TextBox3 As TextBox
     Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TopMenuStrip As MenuStrip
+    Friend WithEvents HelpTopMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutTopMenuItem As ToolStripMenuItem
 End Class
--- WinFormControls/ControlsForm.vb
+++ WinFormControls/ControlsForm.vb
@@ -1,5 +1,24 @@
 ﻿Public Class ControlsForm
-    Private Sub ControlsForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label5.Text &=
+"XXXXXXXXXXXXXXXXXXXXXXXX" & vbNewLine &
+"XXXXXXXXXXXXXXXXXXXXXXXX" & vbNewLine &
+"XXXXXXXXXXXXXXXXXXXXXXXX" & vbNewLine &
+"XXXXXXXXXXXXXXXXXXXXXXXX" & vbNewLine &
+"XXXXXXXXXXXXXXXXXXXXXXXX"
+
+    End Sub
+
+    Private Sub ControlsForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        PictureBox1.Image = My.Resources.cute_baby_kittens_15
+    End Sub
+
+    Private Sub AboutTopMenuItem_Click(sender As Object, e As EventArgs) Handles HelpTopMenuItem.Click
+
+    End Sub
+
+    Private Sub AboutTopMenuItem_Click_1(sender As Object, e As EventArgs) Handles AboutTopMenuItem.Click
 
     End Sub
 End Class
